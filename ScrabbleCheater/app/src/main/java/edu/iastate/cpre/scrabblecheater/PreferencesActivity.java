f02679e0c0afc271dab753e37bc8cb3929499b87
Shauger <dshauger@iastate.edu>
2016-11-10 12:39:27 -0600

    added modified pref activity

--- ScrabbleCheater/app/src/main/java/edu/iastate/cpre/scrabblecheater/PreferencesActivity.java
+++ ScrabbleCheater/app/src/main/java/edu/iastate/cpre/scrabblecheater/PreferencesActivity.java
@@ -1,13 +1,32 @@
+/*
+ *  The preferences activity will allow the user to choose from a few dictionaries (large and small) as
+ *   well as switch between the original Scrabble board and the Words with Friends board.
+ *
+ */
+
 package edu.iastate.cpre.scrabblecheater;
 
-import android.support.v7.app.ActionBarActivity;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class PreferencesActivity extends ActionBarActivity {
-
+public class PreferencesActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_preferences);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new ScrabblePreferencesFragment()).commit();
+    }
+
+    public static class ScrabblePreferencesFragment extends PreferenceFragment{
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+
+            PreferenceManager.setDefaultValues(getActivity(), R.xml.scrabble_preferences, false);
+            addPreferencesFromResource(R.xml.scrabble_preferences);
+        }
     }
 }
