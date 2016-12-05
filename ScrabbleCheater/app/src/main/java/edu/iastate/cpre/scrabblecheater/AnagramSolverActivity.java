81ac969a335a39418e43814d2e08479ff15c63a2
dshauger <dshauger@iastate.edu>
2016-12-05 12:08:52 -0600

    Added preferences menu to AnagramSolverActivity

--- ScrabbleCheater/app/src/main/java/edu/iastate/cpre/scrabblecheater/AnagramSolverActivity.java
+++ ScrabbleCheater/app/src/main/java/edu/iastate/cpre/scrabblecheater/AnagramSolverActivity.java
@@ -7,6 +7,8 @@ import android.content.res.AssetManager;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
@@ -35,6 +37,26 @@ public class AnagramSolverActivity extends ActionBarActivity {
         setContentView(R.layout.activity_anagram_solver);
         editText = (EditText) findViewById(R.id.word_entry);
         editText.setText(getIntent().getStringExtra("tiles"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.scrabble_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_preferences:
+                // Display Settings page
+                startActivity(new Intent(this, PreferencesActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onMatchClick(View v) {
